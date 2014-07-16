cd91cb7131ff9f22368bc7ff3691b84f45a155f0
“yaopeiling” <yaopeiling@xiaomi.com>
2014-07-16 12:51:55 +0800

    picker

--- touch/page/picker.js
+++ touch/page/picker.js
@@ -3,20 +3,23 @@
 	var picker = function(option){
 	   index++;
 	   var me = this;
+	   var max = 12;
 	   var option = option||{};
 	   me.el = $(option.el);
 	   me.onclick = option.onclick || function(){};
 	   var currentValue = me.el.next().val();
 	   var type = me.type = option.type;
        var tplhtml = '<div class="area-list date-list" id="picker_'+index+'">\
-					   <p class="first"><em class="data-corner-left" style="display:none"></em><span class="picker_title">选择日期</span><em class="data-corner-right" style="display:none"></em></p>\
+					   <p class="first"><em class="data-corner-left left_arrow" style="display:none"></em><span class="picker_title">选择日期</span><em class="data-corner-right right_arrow" style="display:none"></em></p>\
 					  <ul>\
 						<%=container%>\
 					  </ul>\
+					  <%=pageHTML%>\
 					</div>';
 	  var container = $('body');
 	 
-
+      this.currentPage = 0;
+	  this.totalPage = 0;
 	  if(type == 'zone'){
 			M.get('route.php?mod=order&action=get_region', {}, function(d) {
 				var html = '';
@@ -32,6 +35,7 @@
 				  html += '<li class="pos-r '+checked+'" data-id="'+d[i].region_id+'"  style="position:relative">' + d[i].region_name + '</li>';
 				}
 				tplhtml = tplhtml.replace('<%=container%>',html);
+				tplhtml = tplhtml.replace('<%=pageHTML%>','');
 				container.append(tplhtml);
 				
 				me.defaultEvent();
@@ -48,16 +52,19 @@
 				  html += '<li class="pos-r '+checked+'" data-id="'+i+'"  style="position:relative">' + d[i].name + '<em class="wap-radio wap-radio-item current '+checked+'"></em></li>';
 			}
 			tplhtml = tplhtml.replace('<%=container%>',html);
+			tplhtml = tplhtml.replace('<%=pageHTML%>','');
 			container.append(tplhtml);
 			
 			me.defaultEvent();
 			me.setTitle('选择街道');
 		}else if(type == 'date'){
 			var html='';
+			var pageHtml=[];
+			var numindex = 0;
 			var startTime = parseInt(window.server_date);
 			currentValue = $('#month_sel').val()+$('#day_sel').val();
-		
 			for (var i = 0; i < 30; i++) {
+				  numindex++;
 				  var _date = new Date(startTime);
 				  var year = _date.getFullYear();
 				  var month = _date.getMonth()+1;
@@ -65,18 +72,35 @@
 				  startTime=startTime+(3600*24*1000);
 				  var display = month+'月'+day+'日';
 				  var checked = '';
+				  var _curhtml = '<li class="pos-r '+checked+'" data-day="'+day+'" data-month="'+month+'" data-year="'+year+'"  style="position:relative">' + display + '</li>';
 				  if(month+''+day == currentValue){
 				     checked = 'current';
 				  }
-				  html += '<li class="pos-r '+checked+'" data-day="'+day+'" data-month="'+month+'" data-year="'+year+'"  style="position:relative">' + display + '</li>';
+				  if(numindex>12){
+					 pageHtml.push(_curhtml);
+				  }else{
+					 html += _curhtml;
+				  }
+			}
+			var _htmlArr= [];
+			while(pageHtml.length){
+			   var tmpHtml=pageHtml.splice(0,12);
+			   _htmlArr.push('<ul style="display:none">');
+			   _htmlArr.push(tmpHtml.join(''));
+			   _htmlArr.push('</ul>');
+			   this.totalPage++;
+
 			}
 			tplhtml = tplhtml.replace('<%=container%>',html);
+			tplhtml = tplhtml.replace('<%=pageHTML%>',_htmlArr.join(''));
 			container.append(tplhtml);
+		
 			
 			me.defaultEvent();
 			me.setTitle('选择送货日期');
 		}else if(type == 'time'){
 			var html='';
+			var numindex = 0;
 			currentValue = $('#hour_sel').val()+$('#minute_sel').val();
 			var begin = option.beginHour;
 			var end = option.endHour;
@@ -102,6 +126,7 @@
 				}
 			}
 			tplhtml = tplhtml.replace('<%=container%>',html);
+			tplhtml = tplhtml.replace('<%=pageHTML%>','');
 			container.append(tplhtml);
 			
 			me.defaultEvent();
@@ -117,6 +142,10 @@
   picker.prototype.defaultEvent = function(){
     var me = this;
 	me.listContainer = $('#picker_'+index);
+	
+	if(this.totalPage>0){
+	  me.listContainer.find('.right_arrow').show();
+	}
 	me.listContainer.delegate('li',CLICK,function(){
 			var _this = $(this);
 			me.el.next().val(_this.data('id'));
@@ -129,8 +158,40 @@
 			}else{
 			  me.onclick(_this.data('id'));
 			}
-			
 	});
+
+	me.listContainer.delegate('.left_arrow',CLICK,function(){
+		me.currentPage--;
+		if(me.currentPage<0){
+		   me.currentPage=0;
+		}else{
+		   me.listContainer.find('.left_arrow').show();
+		}
+		if(me.currentPage == 0){
+		   me.listContainer.find('.left_arrow').hide();
+		}
+
+		 me.listContainer.find('.right_arrow').show();
+		 var ul = me.listContainer.find('ul');
+		 ul.hide();
+		 ul.eq(me.currentPage).show();
+	}).delegate('.right_arrow',CLICK,function(){
+	    me.currentPage++;
+		if(me.currentPage>me.totalPage){
+		   me.currentPage=me.totalPage;
+		}else{
+		   me.listContainer.find('.right_arrow').show();
+		}
+
+		if(me.currentPage == me.totalPage){
+		   me.listContainer.find('.right_arrow').hide();
+		}
+		me.listContainer.find('.left_arrow').show();
+		 
+		 var ul = me.listContainer.find('ul');
+		 ul.hide();
+		 ul.eq(me.currentPage).show();
+	})
   }
   picker.prototype.close = function(){
 	 $('#scroll_container').css('overflow','auto');
